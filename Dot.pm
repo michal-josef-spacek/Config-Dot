21ca2261c17f6a531a4a5bf027a56cf56742efef
Michal Špaček <tupinek@gmail.com>
2011-09-01 08:13:52 +0200

    Improve SYNOPSIS in doc.

--- Dot.pm
+++ Dot.pm
@@ -162,6 +162,7 @@ Config::Dot - Module for simple configure file parsing.
  my $cnf = Config::Dot->new(%params);
  my $struct_hr = $cnf->parse($string);
  $cnf->reset;
+ my $serialized = $cnf->serialize;
 
 =head1 METHODS
 
