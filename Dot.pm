6d53098b549caa54839ae5dacc5bd20ba926152e
Michal Špaček <tupinek@gmail.com>
2014-08-17 11:59:08 +0200

    Prepare for 0.07 version.

--- Dot.pm
+++ Dot.pm
@@ -15,7 +15,7 @@ use Readonly;
 Readonly::Scalar my $EMPTY_STR => q{};
 
 # Version.
-our $VERSION = 0.06;
+our $VERSION = 0.07;
 
 # Constructor.
 sub new {
@@ -375,6 +375,6 @@ BSD license.
 
 =head1 VERSION
 
-0.06
+0.07
 
 =cut
